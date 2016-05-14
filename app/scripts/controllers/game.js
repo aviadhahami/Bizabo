b5753f665b824302283ec4ae66f2236355cda917
Aviad Hahami <aviadhahami@gmail.com>
2016-05-15 01:30:23 +0300

    genereated view and ctrls

---
+++ app/scripts/controllers/game.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bizabooApp.controller:GameCtrl
+ * @description
+ * # GameCtrl
+ * Controller of the bizabooApp
+ */
+angular.module('bizabooApp')
+  .controller('GameCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
