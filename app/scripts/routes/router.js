905a65b7539f341c46ffdd04d482ddd10e1ff190
Aviad Hahami <aviadhahami@gmail.com>
2016-05-15 00:45:14 +0300

    moved routes to another file

---
+++ app/scripts/routes/router.js
@@ -0,0 +1,20 @@
+/**
+ * Created by aviad on 5/15/2016.
+ */
+
+angular.module('bizabooApp').config(function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'views/main.html',
+			controller: 'MainCtrl',
+			controllerAs: 'main'
+		})
+		.when('/about', {
+			templateUrl: 'views/about.html',
+			controller: 'AboutCtrl',
+			controllerAs: 'about'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+});
