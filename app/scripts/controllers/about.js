5c647a7c8dcbdf42b3738d44e1540667af8bb74d
Aviad Hahami <aviadhahami@gmail.com>
2016-05-14 20:57:44 +0300

    starter kit init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bizabooApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bizabooApp
+ */
+angular.module('bizabooApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
