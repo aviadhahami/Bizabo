f692498264947c7ecdd41b92cbac65eb116b696a
Aviad Hahami <aviadhahami@gmail.com>
2016-05-15 18:55:49 +0300

    Added 'getArtist' srvs

---
+++ app/scripts/services/artistsservice.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name bizzaboApp.ArtistsService
+ * @description
+ * # ArtistsService
+ * Service in the bizzaboApp.
+ */
+angular.module('bizzaboApp')
+	.service('ArtistsService', function () {
+		// AngularJS will instantiate a singleton by calling "new" on this function
+		var artists = [
+			'Jack Johnson',
+			'AFI',
+			'The Angelcy',
+			'Chance the Rapper',
+			'The Weekend',
+			'The Fugees',
+			'Erykah Badu',
+			'Chet Faker',
+			'Alt-J',
+			'Flume',
+			'Drake',
+			'Joey Bada$$',
+			'Wilkinson',
+			'I See Stars',
+			'Clean Bandit',
+			'Calle 13',
+			'5 Seconds of Summer',
+			'Kid Cudi',
+			'Garden City Movement',
+			'The Roots'
+		];
+		return{
+			getFullList:function(){
+				return artists;
+			},
+			getRandomArtist: function(){
+				return artists[Math.floor(Math.random() * artists.length)];
+			}
+		}
+	});
