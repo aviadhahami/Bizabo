67ffc6707a155d01f4bb90ca69e27023e14ae9db
Aviad Hahami <aviadhahami@gmail.com>
2016-05-15 21:06:55 +0300

    implemeting itunes api srvs

--- app/scripts/services/itunesapiservice.js
+++ app/scripts/services/itunesapiservice.js
@@ -8,8 +8,34 @@
  * Service in the bizzaboApp.
  */
 angular.module('bizzaboApp')
-  .service('iTunesApiService', function () {
-
-	  // https://itunes.apple.com/search?term=jack+johnson&entity=album
-
-  });
+	.service('iTunesApiService',['$q', '$http', function ($q, $http) {
+		var baseURL = 'http://itunes.apple.com/search?';
+		// ?term=jack+johnson&entity=album&limit=50
+		var queryLimit = 'limit=50';
+		var entityType = 'entity=album';
+		var injectParamsToUrl = function (paramsArr) {
+			var url = baseURL;
+			paramsArr.forEach(function(param){
+				url = url.concat('&'+param);
+			});
+			return url;
+		};
+		return {
+			getAlbums: function(artist){
+				console.log('artis',typeof artist);
+				var deferred = $q.defer();
+				var jsonpParams ={
+					params: {
+						'callback': 'JSON_CALLBACK'
+					}
+				};
+				var jsonpURL =  injectParamsToUrl(['term='+artist.replace(/ /g,'+'),queryLimit,entityType]);
+				$http.jsonp(jsonpURL,jsonpParams).then(function(res){
+					deferred.resolve(res);
+				},function(err){
+					deferred.reject(err);
+				});
+				return deferred.promise;
+			}
+		}
+	}]);
