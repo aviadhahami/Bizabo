8bf54e3a1bf0df66d9bd891c4e5ef0706afd32f3
Aviad Hahami <aviadhahami@gmail.com>
2016-05-16 00:43:38 +0300

    injected modal srvs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,7 +17,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+	  'angularModalService'
   ])
   .config(function ($routeProvider) {
     $routeProvider
