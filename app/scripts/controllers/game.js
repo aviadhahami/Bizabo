f5637aa6cbee62c9ede8d890b22d93bf1d687b24
Aviad Hahami <aviadhahami@gmail.com>
2016-05-15 21:42:45 +0300

    working on game view

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -8,11 +8,22 @@
  * Controller of the bizzaboApp
  */
 angular.module('bizzaboApp')
-	.controller('GameCtrl',['iTunesApiService','ArtistsService', function (iTunesApiService,ArtistsService) {
-		var artist = ArtistsService.getRandomArtist();
-		iTunesApiService.getAlbums(artist).then(function(res){
-			console.log(res)
-		},function(err){
-			console.log(err)
-		})
+	.controller('GameCtrl',['$scope', 'iTunesApiService','ArtistsService', function ($scope, iTunesApiService,ArtistsService) {
+		$scope.albums = [];
+		$scope.round = 0;
+		var init = function(){
+			$scope.round = 1;
+			initRound();
+		};
+		var initRound = function(){
+			$scope.loading = true;
+			iTunesApiService.getAlbums(ArtistsService.getRandomArtist()).then(function(res){
+				console.log(res.data.results);
+				$scope.loading = false;
+			},function(err){
+				console.log(err);
+				alert('ERROR!');
+			})
+		};
+		init();
 	}]);
