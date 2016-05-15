fefb52cdf058dd0f93a3c1906eced8604b71d9de
Aviad Hahami <aviadhahami@gmail.com>
2016-05-15 23:33:37 +0300

    working on end game logic

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -8,98 +8,110 @@
  * Controller of the bizzaboApp
  */
 angular.module('bizzaboApp')
-	.controller('GameCtrl',['$scope', 'iTunesApiService','ArtistsService', function ($scope, iTunesApiService,ArtistsService) {
-		var albums = [];
-		var attempt;
-		var artist;
-		var round;
-		$scope.inputField ='';
+	.controller('GameCtrl',['$scope', '$location','userData','iTunesApiService','ArtistsService',
+		function ($scope, $location, userData,iTunesApiService,ArtistsService) {
 
-		var initGame = function(){
-			round = 0;
-			initRound(round);
-			$scope.score = 0;
-		};
-		
-		var exposeAlbumToScope = function (index) {
-			console.log(albums);
-			console.log('pushing with index',index);
-			$scope.albums.push(albums[index]);
-		};
-
-		var endGame = function () {
-			// TODO: on game end
-			console.log('DONE!')
-		};
-
-		var initRound = function(){
-
-			round++;
-			// Check for game end
-			if(round === 6){
-				endGame();
-				return;
+			// Kick intruders out if didn't click
+			if(!userData.isPlaying()){
+				$location.path('/');
 			}
+			var albums = [];
+			var attempt;
+			var artist;
+			var round;
+			$scope.inputField ='';
 
-			attempt=0;
-			$scope.attemp = attempt;
-			albums = [];
-			$scope.albums = [];
-			$scope.round = round;
-			$scope.currentQuestions = [];
-			$scope.loading = true;
-			artist = ArtistsService.getRandomArtist();
-			iTunesApiService.getThreeAlbums(artist).then(function(res){
-				albums = res;
-				exposeAlbumToScope(attempt);
-				$scope.loading = false;
-			},function(err){
-				console.log(err);
-			});
-		};
+			var initGame = function(){
+				round = 0;
+				initRound(round);
+				$scope.score = 0;
+			};
 
-		var updateScore = function () {
-			switch (attempt){
-				case 0:{
-					$scope.score+=5;
-					break;
-				}
-				case 1:{
-					$scope.score+=3;
-					break;
-				}
-				case 2:{
-					$scope.score+=1;
-					break;
-				}
-				default:{
+			var exposeAlbumToScope = function (index) {
+				console.log(albums);
+				console.log('pushing with index',index);
+				$scope.albums.push(albums[index]);
+			};
 
-					// non-harmful fallback
-					$scope.score = $scope.score;
-				}
-			}
-		};
+			var endGame = function () {
+				// TODO: on game end
+				console.log('DONE!')
+				userData.pushScore($scope.score);
+				userData.endGame();
+			};
 
-		var performAttempt = function(guess){
-			if(guess === artist){
-				updateScore();
-				initRound();
-			}else{
-				attempt++;
-				$scope.attempt = attempt;
-				console.log('attempt',attempt);
-				if(attempt == 3){
-					// Means we're done
-					initRound();
-				}else{
+			var initRound = function(){
+
+				round++;
+				// Check for game end
+				if(round === 6){
+					endGame();
+					return;
+				}
+
+				attempt=0;
+				$scope.attemp = attempt;
+				albums = [];
+				$scope.albums = [];
+				$scope.round = round;
+				$scope.currentQuestions = [];
+				$scope.loading = true;
+				artist = ArtistsService.getRandomArtist();
+				iTunesApiService.getThreeAlbums(artist).then(function(res){
+					albums = res;
 					exposeAlbumToScope(attempt);
-				}
-			}
-		};
+					$scope.loading = false;
+				},function(err){
+					console.log(err);
+				});
+			};
 
-		$scope.guess = function(){
-			if($scope.inputField.replace(/ /g,'').length == 0) return;
-			performAttempt($scope.inputField);
-		};
-		initGame();
-	}]);
+			var updateScore = function () {
+				switch (attempt){
+					case 0:{
+						$scope.score+=5;
+						break;
+					}
+					case 1:{
+						$scope.score+=3;
+						break;
+					}
+					case 2:{
+						$scope.score+=1;
+						break;
+					}
+					default:{
+
+						// non-harmful fallback
+						$scope.score = $scope.score;
+					}
+				}
+			};
+
+			var cleanInput = function () {
+				$scope.inputField = '';
+			};
+			var performAttempt = function(guess){
+				if(guess.toLowerCase() === artist.toLowerCase()){
+					updateScore();
+					initRound();
+					cleanInput();
+				}else{
+					attempt++;
+					$scope.attempt = attempt;
+					console.log('attempt',attempt);
+					if(attempt == 3){
+						// Means we're done
+						initRound();
+					}else{
+						exposeAlbumToScope(attempt);
+					}
+				}
+			};
+
+			$scope.guess = function(){
+				if($scope.inputField.replace(/ /g,'').length == 0) return;
+				performAttempt($scope.inputField);
+			};
+			initGame();
+		}]);
--- app/scripts/services/userdata.js
+++ app/scripts/services/userdata.js
@@ -8,26 +8,39 @@
  * Service in the bizzaboApp.
  */
 angular.module('bizzaboApp')
-  .service('userData', function () {
-	  var user = {};
+	.service('userData', function () {
+		var user = {
+			id:'',
+			isPlaying:'',
+			scores:[]
+		};
 
-	  var generateUserID = function () {
-		  function s4() {
-			  return Math.floor((1 + Math.random()) * 0x10000)
-				  .toString(16)
-				  .substring(1);
-		  }
-		  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-			  s4() + '-' + s4() + s4() + s4();
-	  };
-	  return{
-		  initNewUser: function(){
-			  user.id = generateUserID();
-			  user.isPlaying = false;
-		  },
-		  startGame: function(){
-			  user.isPlaying = true;
-		  }
-		  
-	  };
-  });
+		var generateUserID = function () {
+			function s4() {
+				return Math.floor((1 + Math.random()) * 0x10000)
+					.toString(16)
+					.substring(1);
+			}
+			return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+				s4() + '-' + s4() + s4() + s4();
+		};
+		return{
+			initNewUser: function(){
+				user.id = generateUserID();
+				user.isPlaying = false;
+			},
+			startGame: function(){
+				user.isPlaying = true;
+			},
+			isPlaying: function(){
+				return user.isPlaying;
+			},
+			pushScore: function(score){
+				user.scores.push(score);
+			},
+			endGame : function(){
+				user.isPlaying = false;
+			}
+
+		};
+	});
