22adaa8a2054ead91d8aa8e661a75283633fbe54
Aviad Hahami <aviadhahami@gmail.com>
2016-05-15 19:42:10 +0300

    game view inited

---
+++ app/scripts/controllers/game.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bizzaboApp.controller:GameCtrl
+ * @description
+ * # GameCtrl
+ * Controller of the bizzaboApp
+ */
+angular.module('bizzaboApp')
+  .controller('GameCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
