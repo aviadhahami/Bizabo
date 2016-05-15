ccd23af2462b261aaa048117e2f7550ac991c380
Aviad Hahami <aviadhahami@gmail.com>
2016-05-15 18:43:05 +0300

    re-generated project from official ng template

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bizzaboApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bizzaboApp
+ */
+angular.module('bizzaboApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bizzaboApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bizzaboApp
+ */
+angular.module('bizzaboApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
