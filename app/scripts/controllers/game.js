51f689d198845d0b3a81c04d348984b97a3bed4d
Aviad Hahami <aviadhahami@gmail.com>
2016-05-15 23:02:16 +0300

    debugging

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -9,23 +9,90 @@
  */
 angular.module('bizzaboApp')
 	.controller('GameCtrl',['$scope', 'iTunesApiService','ArtistsService', function ($scope, iTunesApiService,ArtistsService) {
-
+		var albums = [];
+		var attempt;
+		var artist;
+		var round;
+		$scope.inputField ='';
 
 		var initGame = function(){
-			initRound(1);
+			round=1;
+			initRound(round);
+			$scope.score = 0;
+		};
+		
+		var exposeAlbumToScope = function (index) {
+
+			console.log(albums);
+			console.log('pushing with index',index);
+			$scope.albums.push(albums[index]);
 		};
 
-		var initRound = function(roundNum){
+		var endGame = function () {
+			// TODO: on game end
+		};
+		var initRound = function(){
+
+			// Check for game end
+			if(round === 4){
+				endGame();
+				return;
+			}
+
+			attempt=0;
+			albums = [];
 			$scope.albums = [];
-			$scope.round = roundNum;
+			$scope.round = round;
 			$scope.currentQuestions = [];
 			$scope.loading = true;
+			artist = ArtistsService.getRandomArtist();
+			iTunesApiService.getThreeAlbums(artist).then(function(res){
+				albums = res;
+				exposeAlbumToScope(attempt);
+				$scope.loading = false;
+			},function(err){
+				console.log(err);
+			});
+		};
+		var updateScore = function () {
+			switch (attempt){
+				case 0:{
+					$scope.score+=5;
+					break;
+				}
+				case 1:{
+					$scope.score+=3;
+					break;
+				}
+				case 2:{
+					$scope.score+=1;
+					break;
+				}
+				default:{
+
+					// non-harmful fallback
+					$scope.score = $scope.score;
+				}
+			}
+		};
+		var performAttempt = function(guess){
+			if(guess === artist){
+				updateScore();
+			}else{
+				attempt++;
+				console.log('attempt',attempt);
+				if(attempt == 3){
+					// Means we're done
+				}else{
+					exposeAlbumToScope(attempt);
+				}
+			}
+		};
+
+		$scope.guess = function(){
+			if($scope.inputField.replace(/ /g,'').length == 0) return;
+			performAttempt($scope.inputField);
 		};
 
 		initGame();
-		iTunesApiService.getThreeAlbums(ArtistsService.getRandomArtist()).then(function(res){
-			console.log(res);
-		},function(err){
-			console.log(err);
-		})
 	}]);
